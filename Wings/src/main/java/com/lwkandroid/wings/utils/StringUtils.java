56c5cbb90f9f323bf332f7bb1d6de3b0458d728c
luowenkang <505515031@qq.com>
2019-05-06 14:51:24 +0800

    coding

--- Wings/src/main/java/com/lwkandroid/wings/utils/StringUtils.java
+++ Wings/src/main/java/com/lwkandroid/wings/utils/StringUtils.java
@@ -287,33 +287,6 @@ public final class StringUtils
     }
 
     /**
-     * String转ASCII
-     *
-     * @param value 待转换字符串
-     * @return ASCII
-     */
-    public static String stringToAscii(String value)
-    {
-        if (isEmpty(value))
-        {
-            return null;
-        }
-        StringBuffer sbu = new StringBuffer();
-        char[] chars = value.toCharArray();
-        for (int i = 0; i < chars.length; i++)
-        {
-            if (i != chars.length - 1)
-            {
-                sbu.append((int) chars[i]).append(",");
-            } else
-            {
-                sbu.append((int) chars[i]);
-            }
-        }
-        return sbu.toString();
-    }
-
-    /**
      * ASCII转String
      *
      * @param value     ASCII码组成的字符串
